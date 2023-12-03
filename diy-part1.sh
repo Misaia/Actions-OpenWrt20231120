04c7c0a74ae55926a57c827d563886c33f3e7f01
Misaia <3131667831@qq.com>
2023-12-03 13:33:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git smallpackage https://github.com/kenzok8/small-package.git' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
